6ce820d3ffdc8ff6e7fbb6b3f656a4e778172522
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2024-12-08 20:28:40 +0700

    Add Vehicle page from Customer

--- src/shares/components/Layout/Sidebar.js
+++ src/shares/components/Layout/Sidebar.js
@@ -10,6 +10,7 @@ const Sidebar = () => {
                 <li className={pathname === "" ? "active" : ""}><a href="user.html"><svg className="glyph stroked male user "><use xlinkHref="#stroked-male-user" /></svg>Quản lý thành viên</a></li>
                 <li className={pathname === "" ? "active" : ""}><a href="category.html"><svg className="glyph stroked open folder"><use xlinkHref="#stroked-open-folder" /></svg>Quản lý danh mục</a></li>
                 <li className={pathname === "/customers" ? "active" : ""}><Link to="/customers"><svg className="glyph stroked bag"><use xlinkHref="#stroked-bag" /></svg>Quản lý khách hàng</Link></li>
+                <li className={pathname === "/vehicles" ? "active" : ""}><Link to="/vehicles"><svg className="glyph stroked bag"><use xlinkHref="#stroked-bag" /></svg>Quản lý xe</Link></li>
                 <li className={pathname === "" ? "active" : ""}><a href="comment.html"><svg className="glyph stroked two messages"><use xlinkHref="#stroked-two-messages" /></svg> Quản lý bình luận</a></li>
                 <li className={pathname === "" ? "active" : ""}><a href="ads.html"><svg className="glyph stroked chain"><use xlinkHref="#stroked-chain" /></svg> Quản lý quảng cáo</a></li>
                 <li className={pathname === "" ? "active" : ""}><a href="setting.html"><svg className="glyph stroked gear"><use xlinkHref="#stroked-gear" /></svg> Cấu hình</a></li>
